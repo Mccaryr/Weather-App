dfe3c7a07dccc5463e4fddaa3a75549a77376820
Mccaryr <robert.mccary@yahoo.com>
2019-09-02 10:08:24 -0700

    Created Form/Title Components

---
+++ weather-app/src/components/title.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Titles = props => {
+    return (
+        <div>
+        <h1> Weather! </h1> 
+        <p> Accurate Weather 60% of the Time EVERY Time </p>
+        </div>
+    )
+}
+
+export default Titles;
