1f8eae694812037afff07dfa5afc3504c833d821
Mccaryr <robert.mccary@yahoo.com>
2019-09-03 09:18:54 -0700

    created container components and adjusted kelvin to fahrenheight

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
 
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${Api_Key}`);
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=imperial&appid=${Api_Key}`);
     
     const response = await api_call.json();
     
