ea0bf21c4db8084fbc406f9736272ba80b776bd7
Mccaryr <robert.mccary@yahoo.com>
2019-09-24 12:14:25 -0700

    debugging

--- weather-app/src/actions/fetchweeklyweather.js
+++ weather-app/src/actions/fetchweeklyweather.js
@@ -10,7 +10,7 @@ export const fetchWeeklyWeather = (city, country) => {
         return fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&units=imperial&appid=${Api_Key}`)
         .then(response => response.json())
         .then(resp => {
-            console.log(resp.list)
+            console.log(resp.list[0].main)
             return resp 
         })
         .then(data => dispatch({type: "FETCH_WEEKLYWEATHER", weather: data.list }))
