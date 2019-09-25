f0ec5aa159a17c4d01fb9e62f93c0657d6732063
Mccaryr <robert.mccary@yahoo.com>
2019-09-25 14:25:49 -0700

    Setup Routes and Controllers for Back-End

--- weather-backend/app/controllers/api/v1/locations_controller.rb
+++ weather-backend/app/controllers/api/v1/locations_controller.rb
@@ -1,2 +1,32 @@
 class Api::V1::LocationsController < ApplicationController
+
+    def index 
+        @locations = Location.all 
+        render json: @locations 
+    end 
+
+    def create 
+        @location = Location.new(location_params)
+        if @location.save 
+            render json: @location 
+        else 
+            render json: {error: 'Error with Location'}
+        end 
+    end 
+
+    def show 
+        @location = Location.find(params[:id])
+        render json: @location 
+    end 
+
+    def destroy 
+        @location = Location.find(params[:id])
+        @location.destroy 
+    end 
+
+    private 
+
+    def location_params
+        params.require(:location).permit(:city, :country)
+    end 
 end
--- weather-backend/app/controllers/api/v1/weathers_controller.rb
+++ weather-backend/app/controllers/api/v1/weathers_controller.rb
@@ -1,2 +1,35 @@
 class Api::V1::WeathersController < ApplicationController
+    before_action :set_location
+
+    def index 
+      @weathers = @location.weathers
+      render json: @weathers  
+    end 
+
+    def create 
+        @weather = @location.weathers.build(weather_params)
+        if @weather.save 
+            render json: @weather
+        else 
+            render json: {error: 'No Weather Data'}
+        end  
+    end 
+
+    def show 
+        @weather = @location.weathers.find_by(id: params[:id])
+        render json: @weather
+    end 
+
+    def destroy 
+    end 
+
+    private 
+
+    def weather_params
+        params.require(:weather).permit(:temperature, :location_id)
+    end 
+
+    def set_location
+        @location = Location.find(params[:location_id])
+    end 
 end
